cf9c965bcc442c8eb1481e6ccca2f4afc6d95834
asimov4 <asimov4@gmail.com>
2012-05-14 18:31:56 -0700

    les livrables sont importes depuis le moderator

---
+++ livrables.php
@@ -0,0 +1,6 @@
+﻿<?php
+$completeurl = file_get_contents("http://www.googleapis.com/moderator/v1/series/750605/topics/74/submissions?sort=RANK_DESCENDING&max-results=6");
+
+echo $completeurl;
+
+?>
